eed6b7666842ce9473a23c85e0af54b42ad9ffef
Indra Pramana <indra@ide.web.id>
2020-08-14 17:10:44 +0700

    first commit

---
+++ database/migrations/2020_08_14_084214_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id');
+            $table->foreignId('distributor_id');
+            $table->foreignId('unit_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
